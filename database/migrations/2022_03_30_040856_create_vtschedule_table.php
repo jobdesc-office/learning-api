0675e9949b94fabeb3e2c7657558b4c2cc58e5c7
NovanHyperData <novan@hyperdatasolusindo.com>
2022-03-30 13:24:54 +0700

    Add New Table

---
+++ database/migrations/2022_03_30_040856_create_vtschedule_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVtscheduleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vtschedule', function (Blueprint $table) {
+            $table->id('scheid');
+            $table->string('schenm', 50)->nullable();
+            $table->date('schestartdate')->nullable();
+            $table->date('scheenddate')->nullable();
+            $table->date('schestarttime')->nullable();
+            $table->date('scheendtime')->nullable();
+            $table->integer('schetypeid')->nullable();
+            $table->date('scheactdate')->nullable();
+            $table->integer('schetowardid')->nullable();
+            $table->integer('schebpid')->nullable();
+            $table->integer('schereftypeid')->nullable();
+            $table->integer('scherefid')->nullable();
+            $table->boolean('scheallday')->nullable();
+            $table->text('scheloc')->nullable();
+            $table->boolean('scheprivate')->nullable();
+            $table->boolean('scheonline')->nullable();
+            $table->text('schetz')->nullable();
+            $table->integer('scheremind')->nullable();
+            $table->text('schedesc')->nullable();
+            $table->text('scheonlink')->nullable();
+
+            $table->bigInteger('createdby')->nullable();
+            $table->timestamp('createddate')->useCurrent();
+            $table->bigInteger('updatedby')->nullable();
+            $table->timestamp('updateddate')->useCurrent();
+            $table->boolean('isactive')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vtschedule');
+    }
+}
