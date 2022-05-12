edff9288d9410eb9000a6873fe4eb16a856e073f
--replace-all <--replace-all>
2022-05-12 16:33:33 +0700

    create controller for store customer data and upload picture

--- app/Http/Controllers/Masters/BpCustomerController.php
+++ app/Http/Controllers/Masters/BpCustomerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Masters;
 use App\Http\Controllers\Controller;
 use App\Services\Masters\BpCustomerService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class BpCustomerController extends Controller
 {
@@ -18,11 +19,25 @@ class BpCustomerController extends Controller
 
     public function store(Request $req, BpCustomerService $modelBpCustomerService)
     {
-        $insert = collect($req->only($modelBpCustomerService->getFillable()))->filter();
 
-        $modelBpCustomerService->create($insert->toArray());
+        $insert = collect($req->all())->filter();
 
-        return response()->json(['message' => \TextMessages::successCreate]);
+        $image = $req->file('sbccstmpic');
+        if ($image != null) {
+            $filename = Str::random(25) . $image->getClientOriginalName();
+            $res = $image->storeAs('public/images', $filename);
+            if ($res) {
+                $insert->put('sbccstmpic', url('/storage/images/' . $filename));
+            }
+        }
+
+        $result = $modelBpCustomerService->createCustomer($insert);
+
+        if ($result) {
+            return response()->json(['message' => \TextMessages::successCreate,]);
+        } else {
+            return response()->json(['message' => \TextMessages::failedCreate,], 400);
+        }
     }
 
     public function show($id, BpCustomerService $businessPartnerService)
