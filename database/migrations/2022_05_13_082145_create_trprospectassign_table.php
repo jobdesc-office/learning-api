eb4ecd009ec4efe595b8fa9fb96989bbcfeba1bc
NovanHyperData <novan@hyperdatasolusindo.com>
2022-05-13 15:23:50 +0700

    Create New Migration trprospectassign

---
+++ database/migrations/2022_05_13_082145_create_trprospectassign_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrprospectassignTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trprospectassign', function (Blueprint $table) {
+            $table->id('prospectassignid');
+            $table->integer('prospectassignto');
+            $table->integer('prospectreportto');
+            $table->text('prospectdescription');
+
+            $table->bigInteger('createdby')->nullable();
+            $table->timestamp('createddate')->useCurrent();
+            $table->bigInteger('updatedby')->nullable();
+            $table->timestamp('updateddate')->useCurrent();
+            $table->boolean('isactive')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trprospectassign');
+    }
+}
