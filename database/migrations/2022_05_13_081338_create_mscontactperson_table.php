8ddf5570166529d68e63fccd06b2dfa3ea81001e
NovanHyperData <novan@hyperdatasolusindo.com>
2022-05-13 15:19:57 +0700

    Create New Migration mscontactperson

---
+++ database/migrations/2022_05_13_081338_create_mscontactperson_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMscontactpersonTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mscontactperson', function (Blueprint $table) {
+            $table->id('contactpersonid');
+            $table->integer('customerid');
+            $table->integer('contacttypeid');
+            $table->text('contactvalueid');
+
+            $table->bigInteger('createdby')->nullable();
+            $table->timestamp('createddate')->useCurrent();
+            $table->bigInteger('updatedby')->nullable();
+            $table->timestamp('updateddate')->useCurrent();
+            $table->boolean('isactive')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mscontactperson');
+    }
+}
