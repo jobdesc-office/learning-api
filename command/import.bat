3dcb282fca3c914c72733a249f844027709ff0d1
Revann <rghofur053@gmail.com>
2022-05-26 08:39:10 +0700

    setup for ubuntu

--- command/import.bat
+++
@@ -1,4 +0,0 @@
-psql -c "\copy mscountry FROM 'csv_backup/country.csv' delimiter ',' csv header;" "host=127.0.0.1 port=5432 dbname=ventes user=postgres password=revan1853"
-psql -c "\copy msprovince FROM 'csv_backup/province.csv' delimiter ',' csv header;" "host=127.0.0.1 port=5432 dbname=ventes user=postgres password=revan1853"
-psql -c "\copy mscity FROM 'csv_backup/city.csv' delimiter ',' csv header;" "host=127.0.0.1 port=5432 dbname=ventes user=postgres password=revan1853"
-psql -c "\copy mssubdistrict FROM 'csv_backup/subdistrict.csv' delimiter ',' csv header;" "host=127.0.0.1 port=5432 dbname=ventes user=postgres password=revan1853"
