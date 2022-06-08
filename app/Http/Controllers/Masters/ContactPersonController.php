e5388a6a4885b3c4e21ec43bef9c1f7e493485d2
NovanHyperData <novan@hyperdatasolusindo.com>
2022-06-08 11:52:41 +0700

    Add ContactPerson Controller for Web

---
+++ app/Http/Controllers/Masters/ContactPersonController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Masters;
+
+use App\Http\Controllers\Controller;
+use App\Services\Masters\ContactPersonServices;
+use App\Services\Masters\SubdistrictServices;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ContactPersonController extends Controller
+{
+    public function datatables(ContactPersonServices $contactPersonServices)
+    {
+        // $order = $req->get('order[0][column]');
+        // $orderby = $req->get('order[0][dir]');
+
+        // if ($order == 0) {
+        //     $order = $req->get('columns[0][data]');
+        // } elseif ($order == 1) {
+        //     $order = $req->get('columns[1][data]');
+        // } elseif ($order == 2) {
+        //     $order = $req->get('columns[2][data]');
+        // } elseif ($order == 3) {
+        //     $order = $req->get('columns[3][data]');
+        // } elseif ($order == 4) {
+        //     $order = $req->get('columns[4][data]');
+        // } elseif ($order == 5) {
+        //     $order = $req->get('columns[5][data]');
+        // } elseif ($order == 6) {
+        //     $order = $req->get('columns[6][data]');
+        // } elseif ($order == 7) {
+        //     $order = $req->get('columns[7][data]');
+        // } elseif ($order == 8) {
+        //     $order = $req->get('columns[8][data]');
+        // } elseif ($order == 9) {
+        //     $order = $req->get('columns[9][data]');
+        // } else {
+        //     $order = $order;
+        // }
+        $query = $contactPersonServices->datatables();
+
+        return datatables()->eloquent($query)
+            ->toJson();
+    }
+
+    public function all(Request $req, ContactPersonServices $prospectService)
+    {
+        $whereArr = collect($req->all())->filter();
+        $businesspartners = $prospectService->getAll($whereArr);
+        return response()->json($businesspartners);
+    }
+
+    public function store(Request $req, ContactPersonServices $modelContactPersonServices)
+    {
+        $insert = collect($req->only($modelContactPersonServices->getFillable()))->filter()
+            ->except('updatedby');
+
+        $modelContactPersonServices->create($insert->toArray());
+
+        return response()->json(['message' => \TextMessages::successCreate]);
+    }
+
+    public function show($id, ContactPersonServices $businessPartnerService)
+    {
+        $row = $businessPartnerService->find($id);
+        return response()->json($row);
+    }
+
+    public function update($id, Request $req, ContactPersonServices $modelContactPersonServices)
+    {
+        $row = $modelContactPersonServices->findOrFail($id);
+
+        $update = collect($req->only($modelContactPersonServices->getFillable()))->filter()
+            ->except('createdby');
+        $row->update($update->toArray());
+
+        return response()->json(['message' => \TextMessages::successEdit]);
+    }
+
+    public function destroy($id, ContactPersonServices $modelContactPersonServices, SubdistrictServices $subdistrictservice)
+    {
+        DB::beginTransaction();
+        try {
+            $subdistrictservice->select('subdistrictcityid')->where('subdistrictcityid', $id)->delete();
+            $modelContactPersonServices->findOrFail($id)->delete();
+            DB::commit();
+            return response()->json(['message' => \TextMessages::successDelete]);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+        }
+    }
+}
