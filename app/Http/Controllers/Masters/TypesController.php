fe25a651795a00d09d131833bd13614b31a4332e
NovanHyperData <novan@hyperdatasolusindo.com>
2022-06-09 13:36:43 +0700

    Change Else If to Switch Case

--- app/Http/Controllers/Masters/TypesController.php
+++ app/Http/Controllers/Masters/TypesController.php
@@ -21,28 +21,48 @@ class TypesController extends Controller
         $order = $req->get('order[0][column]');
         $orderby = $req->get('order[0][dir]');
 
-        if ($order == 0) {
-            $order = $req->get('columns[0][data]');
-        } elseif ($order == 1) {
-            $order = $req->get('columns[1][data]');
-        } elseif ($order == 2) {
-            $order = $req->get('columns[2][data]');
-        } elseif ($order == 3) {
-            $order = $req->get('columns[3][data]');
-        } elseif ($order == 4) {
-            $order = $req->get('columns[4][data]');
-        } elseif ($order == 5) {
-            $order = $req->get('columns[5][data]');
-        } elseif ($order == 6) {
-            $order = $req->get('columns[6][data]');
-        } elseif ($order == 7) {
-            $order = $req->get('columns[7][data]');
-        } elseif ($order == 8) {
-            $order = $req->get('columns[8][data]');
-        } elseif ($order == 9) {
-            $order = $req->get('columns[9][data]');
-        } else {
-            $order = $order;
+        switch ($order) {
+            case 0:
+                $order = $req->get('columns[0][data]');
+                break;
+            case 1:
+                $order = $req->get('columns[1][data]');
+                break;
+            case 2:
+                $order = $req->get('columns[2][data]');
+                break;
+
+            case 3:
+                $order = $req->get('columns[3][data]');
+                break;
+
+            case 4:
+                $order = $req->get('columns[4][data]');
+                break;
+
+            case 5:
+                $order = $req->get('columns[5][data]');
+                break;
+
+            case 6:
+                $order = $req->get('columns[6][data]');
+                break;
+
+            case 7:
+                $order = $req->get('columns[7][data]');
+                break;
+
+            case 8:
+                $order = $req->get('columns[8][data]');
+                break;
+
+            case 9:
+                $order = $req->get('columns[9][data]');
+                break;
+
+            default:
+                $order = $order;
+                break;
         }
         $query = $typeServices->datatables($order, $orderby);
 
