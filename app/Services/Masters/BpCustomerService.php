b3e78ba51773218c555e0d287832a3af6e07132a
NovanHyperData <novan@hyperdatasolusindo.com>
2022-06-10 16:22:56 +0700

    Delete Something

--- app/Services/Masters/BpCustomerService.php
+++ app/Services/Masters/BpCustomerService.php
@@ -20,13 +20,9 @@ class BpCustomerService extends BpCustomer
             ->get();
     }
 
-    public function datatables($order, $orderby, $search)
+    public function datatables()
     {
-        return $this->getQuery()
-            ->where(function ($query) use ($search, $order) {
-                $query->where(DB::raw("TRIM(LOWER($order))"), 'like', "%$search%");
-            })
-            ->orderBy($order, $orderby);
+        return $this->getQuery();
     }
 
     public function find($id)
