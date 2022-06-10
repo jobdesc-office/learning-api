fe4b94345c3400c8d1c1735b600a2e00d2cb9c88
NovanHyperData <novan@hyperdatasolusindo.com>
2022-06-10 16:25:30 +0700

    Delete Something

--- app/Http/Controllers/Masters/ContactPersonController.php
+++ app/Http/Controllers/Masters/ContactPersonController.php
@@ -10,59 +10,14 @@ use Illuminate\Support\Facades\DB;
 
 class ContactPersonController extends Controller
 {
-    public function datatables(Request $req, ContactPersonServices $contactPersonServices)
+    public function datatables(ContactPersonServices $contactPersonServices)
     {
-        $search = trim(strtolower($req->get('search[value]')));
-        $order = $req->get('order[0][column]');
-        $orderby = $req->get('order[0][dir]');
+        $query = $contactPersonServices->datatables();
 
-        switch ($order) {
-            case 0:
-                $order = $req->get('columns[0][data]');
-                break;
-            case 1:
-                $order = $req->get('columns[1][data]');
-                break;
-            case 2:
-                $order = $req->get('columns[2][data]');
-                break;
-
-            case 3:
-                $order = $req->get('columns[3][data]');
-                break;
-
-            case 4:
-                $order = $req->get('columns[4][data]');
-                break;
-
-            case 5:
-                $order = $req->get('columns[5][data]');
-                break;
-
-            case 6:
-                $order = $req->get('columns[6][data]');
-                break;
-
-            case 7:
-                $order = $req->get('columns[7][data]');
-                break;
-
-            case 8:
-                $order = $req->get('columns[8][data]');
-                break;
-
-            case 9:
-                $order = $req->get('columns[9][data]');
-                break;
-
-            default:
-                $order = $order;
-                break;
-        }
-        $query = $contactPersonServices->datatables($order, $orderby, $search);
-
-        return datatables()->eloquent($query)
-            ->toJson();
+        return
+            datatables()->eloquent($query)
+            ->toJson()
+            ->getOriginalContent();
     }
 
     public function all(Request $req, ContactPersonServices $prospectService)
