334ddadd6a3d76c31d40c02bcacd4e6a7af5ba9b
NovanHyperData <novan@hyperdatasolusindo.com>
2022-06-10 16:32:14 +0700

    Delete Something

--- app/Http/Controllers/Masters/TypesController.php
+++ app/Http/Controllers/Masters/TypesController.php
@@ -18,57 +18,12 @@ class TypesController extends Controller
 
     public function datatables(Request $req, TypeServices $typeServices)
     {
-        $search = trim(strtolower($req->get('search[value]')));
-        $order = $req->get('order[0][column]');
-        $orderby = $req->get('order[0][dir]');
+        $query = $typeServices->datatables();
 
-        switch ($order) {
-            case 0:
-                $order = $req->get('columns[0][data]');
-                break;
-            case 1:
-                $order = $req->get('columns[1][data]');
-                break;
-            case 2:
-                $order = $req->get('columns[2][data]');
-                break;
-
-            case 3:
-                $order = $req->get('columns[3][data]');
-                break;
-
-            case 4:
-                $order = $req->get('columns[4][data]');
-                break;
-
-            case 5:
-                $order = $req->get('columns[5][data]');
-                break;
-
-            case 6:
-                $order = $req->get('columns[6][data]');
-                break;
-
-            case 7:
-                $order = $req->get('columns[7][data]');
-                break;
-
-            case 8:
-                $order = $req->get('columns[8][data]');
-                break;
-
-            case 9:
-                $order = $req->get('columns[9][data]');
-                break;
-
-            default:
-                $order = $order;
-                break;
-        }
-        $query = $typeServices->datatables($order, $orderby, $search);
-
-        return datatables()->eloquent($query)
-            ->toJson();
+        return
+            datatables()->eloquent($query)
+            ->toJson()
+            ->getOriginalContent();
     }
 
     public function store(Request $req, Types $modelTypes)
--- app/Services/Masters/TypeServices.php
+++ app/Services/Masters/TypeServices.php
@@ -17,13 +17,9 @@ class TypeServices extends Types
             ->get();
     }
 
-    public function datatables($order, $orderby, $search)
+    public function datatables()
     {
-        return $this->newQuery()->select('*')->where('typemasterid', null)
-            ->where(function ($query) use ($search, $order) {
-                $query->where(DB::raw("TRIM(LOWER($order))"), 'like', "%$search%");
-            })
-            ->orderBy($order, $orderby);
+        return $this->newQuery()->select('*');
     }
 
     public function find($id)
