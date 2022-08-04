ab6eddd687ff793b4d75bf0e6956b52e7c56b8b2
NovanHyperData <novan@hyperdatasolusindo.com>
2022-08-04 08:10:09 +0700

    Add BySeq

--- routes/web.php
+++ routes/web.php
@@ -107,6 +107,7 @@ $router->group(['middleware' => 'auth'], function () use ($router) {
 
         $router->group(['prefix' => 'types'], function () use ($router) {
             $router->get('by-code', 'TypesController@byCode');
+            $router->get('bySeq', 'TypesController@stage');
             $router->post('datatables', 'TypesController@datatables');
 
             $router->post('', 'TypesController@store');
