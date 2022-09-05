9572f620ee8de0a55e935ad9bac586fb363fd9da
NovanHyperData <novan@hyperdatasolusindo.com>
2022-09-05 11:54:54 +0700

    Create New Migration stbptype

---
+++ database/migrations/2022_09_05_115303_create_stbptype_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStbptypeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stbptype', function (Blueprint $table) {
+            $table->id('sbtid');
+            $table->bigInteger('sbtbpid');
+            $table->text('sbtname');
+            $table->bigInteger('sbttypemasterid');
+            $table->text('sbttypename');
+
+            $table->bigInteger('createdby')->nullable();
+            $table->timestamp('createddate')->useCurrent();
+            $table->bigInteger('updatedby')->nullable();
+            $table->timestamp('updateddate')->useCurrent();
+            $table->boolean('isactive')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stbptype');
+    }
+}
