6d26112022d3602accd0c3248127cc0392443e05
NovanHyperData <novan@hyperdatasolusindo.com>
2022-09-30 13:14:11 +0700

    Delete Permission CSV

--- command/importlocal.bat
+++ command/importlocal.bat
@@ -4,10 +4,6 @@ psql -c "\copy mscity FROM 'csv_backup/city.csv' delimiter ',' csv header;" "hos
 psql -c "\copy mssubdistrict FROM 'csv_backup/subdistrict.csv' delimiter ',' csv header;" "host=localhost port=5432 dbname=ventes user=postgres password=revan1853"
 psql -c "\copy msvillage FROM 'csv_backup/village.csv' delimiter ',' csv header;" "host=localhost port=5432 dbname=ventes user=postgres password=revan1853"
 
-psql -c "\copy msmenu FROM 'permission/menu.csv' delimiter ',' csv header;" "host=10.21.1.30 port=5432 dbname=ventes user=postgres password=revan1853"
-psql -c "\copy msfeature FROM 'permission/feature.csv' delimiter ',' csv header;" "host=10.21.1.30 port=5432 dbname=ventes user=postgres password=revan1853"
-psql -c "\copy mspermission FROM 'permission/permission.csv' delimiter ',' csv header;" "host=10.21.1.30 port=5432 dbname=ventes user=postgres password=revan1853"
-
 @REM psql -c "\copy dspbybp FROM 'dsp/dspbybp.csv' delimiter ',' csv header;" "host=10.21.1.30 port=5432 dbname=ventes user=postgres password=revan1853"
 @REM psql -c "\copy dspbycust FROM 'dsp/dspbycust.csv' delimiter ',' csv header;" "host=10.21.1.30 port=5432 dbname=ventes user=postgres password=revan1853"
 @REM psql -c "\copy dspbycustlabel FROM 'dsp/dspbycustlabel.csv' delimiter ',' csv header;" "host=10.21.1.30 port=5432 dbname=ventes user=postgres password=revan1853"
